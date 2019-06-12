50c7fe83b85d8afd569cc806217321e117f4dbaa
daslu <daniel.slutsky@gmail.com>
2019-06-12 15:03:03 +0300

    project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojuress "0.1.0-SNAPSHOT"
+(defproject clojuress "0.1.0"
   :description "Clojure<->R interop"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
