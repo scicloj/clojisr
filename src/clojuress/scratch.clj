d78477195695f74e41b591e646baf90cf9038c56
daslu <daniel.slutsky@gmail.com>
2019-09-08 23:27:12 +0300

    cleanup

--- src/clojuress/scratch.clj
+++ src/clojuress/scratch.clj
@@ -1,9 +1,5 @@
 (ns clojuress.scratch
-  (:require [clojuress.protocols :refer [eval-r->java]]
-            [clojuress.session :as session]
-            [clojuress.core :as r :refer [r]]
-            [clojuress.impl.rserve.java :as java]
-            [clojuress.protocols :as prot]
+  (:require [clojuress.core :as r :refer [r]]
             [clojuress.packages.base :as base]
             [clojuress.packages.stats :as stats])
   (:import (org.rosuda.REngine REXP REXPSymbol REXPDouble REXPInteger)
@@ -35,7 +31,7 @@
 
   (->> result1
        r/r->java
-       (.asDoubles)
+       r/java->clj
        (take 9)
        (= (map #(double (* % %)) (range 9)))
        time)
@@ -44,17 +40,17 @@
        r/r->java
        r/java->r
        r/r->java
-       (.asDoubles)
+       r/java->clj
        (take 9)
        (= (map #(double (* % %)) (range 9)))
        time)
 
-  (def result1
+  (def result2
     (-> "data.frame(a=1:10000000, b=rnorm(10000000))"
         r
         time))
 
-  (-> result1
+  (-> result2
       ((juxt r/class
              r/names
              r/shape))
@@ -62,196 +58,8 @@
           ["a" "b"]
           [10000000 2]]))
 
-  (require '[clojuress.functions :as f])
-
-  (->> (r "-5:5")
-       f/abs)
-
-
-  (->> (range -5 5)
-       f/abs)
-
-  (->> (r "-5:5")
-       ((r/function (r "abs"))))
-
-  (->> 10000000
-       f/rnorm
-       ((r/function (r "function(x) x*x")))
-       f/mean)
-
-
-  (r/apply-function (r "abs")
-                    [(r "-5:5")]
-                    (session/get {}))
-
-  (->> [(r "-5:5")]
-       (r/apply-function (r "abs")))
-
-  (r "library(ggplot2)")
-
-
-
-  (->> (r "1000")
-       f/rnorm
-       ((r/function (r "(function(x) print(qplot(x)))"))))
-
-  (r "print(qplot(rnorm(1000)))")
-
-  (r "dev.off()")
-
-
 )
 
 
 
 
-
-  (comment
-
-    (r/init)
-
-    (.parseAndEval
-     (:r-connection (session/get {}))
-     "l<-list()")
-
-    (-> (.parseAndEval
-         (:r-connection (session/get {}))
-         "l")
-        (.asList))
-
-    (.parseAndEval
-     (:r-connection (session/get {}))
-     "l$a<-3")
-
-    (-> (.parseAndEval
-         (:r-connection (session/get {}))
-         "l$a")
-        (.asDouble))
-
-
-    (def result1
-      (time
-       (r/eval "(1:10000000)^2")))
-
-    (time
-     (def java-result1
-       (r/r->java result1)))
-
-    (-> result1
-        r/r->java
-        r/java->r
-        r/r->java
-        (.asDoubles)
-        (->> (take 9)))
-
-
-    (setvar (session/get {})
-            "x" (java/doubles [1 2 3]))
-
-
-    (-> (getvar (session/get {})
-                "x")
-        (.asDoubles))
-
-    (setvar (session/get {})
-            "l" (eval-r->java (session/get {}) "list(a=1)"))
-
-    (setvar (session/get {})
-            "l$b" (java/doubles [4 5 6]))
-
-    (-> (getvar (session/get {})
-                "l$a")
-        (.asDoubles))
-
-    (-> (getvar (session/get {})
-                "l$b")
-        (.asDoubles))
-
-    (r/init)
-
-
-
-    (let [conn (-> {}
-                   session/get
-                   :r-connection)]
-      (.parseAndEval conn "{.memory$abcd <- 1 ; 'ok'}")
-      (-> (.parseAndEval conn ".memory$abcd")
-          (.asDoubles)))
-
-
-    (let [conn (-> {}
-                   session/get
-                   :r-connection)]
-      (.eval conn
-             (REXP/asCall "<-"
-                          [(REXPSymbol. "abcd")
-                           (REXPDouble. (double-array [1 2 3]))])
-             nil
-             false)
-      #_(-> (.parseAndEval conn ".memory$abcd")
-            (.asDoubles)))
-
-
-    (-> (REXP/asCall "<-"
-                     (REXPSymbol. "abcd")
-                     (REXPDouble. (double-array [1 2 3]))))
-
-
-    (let [conn (-> {}
-                   session/get
-                   :r-connection)]
-      (.eval
-       conn
-       (java/call "<-"
-                 {}
-                 [(java/call "$"
-                            {}
-                            [(java/symbol ".memory")
-                             (java/symbol "abcd")])
-                  (java/doubles [1 2 3 4 5])])
-       nil
-       true))
-
-
-
-    (let [conn (-> {}
-                   session/get
-                   :r-connection)]
-      (-> (.eval
-           conn
-           ".memory$abcd")
-          (.asDoubles)))
-
-
-
-    (let [conn (-> {}
-                   session/get
-                   :r-connection)]
-      (.eval
-       conn
-       (REXP/asCall
-        "{"
-        (into-array REXP
-                    [(REXP/asCall "<-"
-                                  (REXPSymbol. "X")
-                                  (REXPDouble. (double-array [1 2 3])))
-                     (REXP/asCall "::"
-                                  (REXPSymbol. "base")
-                                  (REXPSymbol. "length")
-                                  (REXPSymbol. "X"))]))
-       nil
-       false))
-
-
-
-
-
-
-
-
-
-
-
-
-
-    )
