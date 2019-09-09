b3cde80471b6c4ec7b7815f0678c6cf91faad8bf
daslu <daniel.slutsky@gmail.com>
2019-09-09 19:41:32 +0300

    tests

--- src/clojuress/util.clj
+++ src/clojuress/util.clj
@@ -21,19 +21,58 @@
 
 (defn fmap
   "fmap updates the values of a given map
-  using a given function
+  using a given function.
  
   (->> {:a 1
         :b 2}
        (fmap inc))
   => {:a 2
       :b 3}"
-  {:added "0.1"} [f m]
+  {:added "0.1"}
+  [f m]
   (specter/transform [specter/MAP-VALS]
                      f
                      m))
 
 
-(defn starts-with? [s v]
+(defn starts-with?
+  "starts-with? checks if a given object s
+  is sequential and its first element
+  equals a given object v.
+ 
+  (starts-with? 4 4)
+  => false
+ 
+  (starts-with? [4] 4)
+  => true
+ 
+  (starts-with? [4] 5)
+  => false
+ 
+  (starts-with? [] 5)
+  => false
+ 
+  (starts-with? [4 5] 4)
+  => true
+ 
+  (starts-with? [4 5] 5)
+  => false
+ 
+  (starts-with? '(4) 4)
+  => true
+ 
+  (starts-with? '(4) 5)
+  => false
+ 
+  (starts-with? '() 5)
+  => false
+ 
+  (starts-with? '(4 5) 4)
+  => true
+ 
+  (starts-with? '(4 5) 5)
+  => false"
+  {:added "0.1"} [s v]
   (and (sequential? s)
        (-> s first (= v))))
+
