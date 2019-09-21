0d3635c7356348ea0123cc9891a9f46f43ce3cb0
daslu <daniel.slutsky@gmail.com>
2019-09-22 01:06:52 +0300

    updated project coordinates

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojuress "0.1.0-SNAPSHOT"
+(defproject scicloj/clojuress "0.1.0-SNAPSHOT"
   :description "Clojure<->R interop"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
