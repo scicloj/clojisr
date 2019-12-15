fb1901f3f7c14f0db5940043b2046f49656e564b
daslu <daniel.slutsky@gmail.com>
2019-12-15 08:35:31 +0200

    deps

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :repositories [["bedatadriven" {:url "https://nexus.bedatadriven.com/content/groups/public/"}]]
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.renjin/renjin-script-engine "0.9.2726"]
                  [gg4clj "0.1.0"]
                  [hara/test "3.0.7"]
                  [hara/code "3.0.7"]
@@ -13,13 +12,11 @@
                  [hara/module.namespace "3.0.7"]
                  [hara/deploy "3.0.7"]
                  [nrepl "0.6.0"]
-                 [com.rpl/specter "1.1.2"]
-                 [techascent/tech.datatype "4.14"]
+                 [com.rpl/specter "1.1.3"]
+                 [techascent/tech.datatype "4.58"]
                  [techascent/tech.resource "4.5"]
-                 [techascent/tech.ml.dataset "1.24"]
+                 [techascent/tech.ml.dataset "1.60"]
                  [org.rosuda.REngine/REngine "2.1.0"]
                  [org.rosuda.REngine/Rserve "1.8.1"]
                  [hiccup "1.0.5"]]
-  :injections [(require 'hara.tool)]
-  ;; :repl-options {:init-ns clojuress.core}
-  )
+  :injections [(require 'hara.tool)])
