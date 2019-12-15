67bbb79aa07b89db97b3faa5057fb3c6e2c3d175
daslu <daniel.slutsky@gmail.com>
2019-12-15 08:51:20 +0200

    changed project coordinates

--- examples/project.clj
+++ examples/project.clj
@@ -1,4 +1,4 @@
-(defproject examples "0.1.0-SNAPSHOT"
+(defproject examples "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
