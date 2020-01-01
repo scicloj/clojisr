e1c46438286a170e346c07d6ef9448bea8c0802a
daslu <daniel.slutsky@gmail.com>
2020-01-02 00:22:26 +0200

    adapted shiny example

--- examples/src/examples/shiny.clj
+++ examples/src/examples/shiny.clj
@@ -1,13 +1,16 @@
 (ns examples.shiny
-  (:require [clojuress :as r :refer [r]]
-            [clojuress.packages.shiny :as s]
-            [clojuress.packages.base :as base]))
+  (:require [clojuress.v0.r :as r :refer [r]]
+            [clojuress.v0.util :refer [l]]
+            [clojuress.v0.require :refer [require-r]]))
+
+(require-r '[base]
+           '[shiny :as s])
 
 ;; Shiny's hello-world
 ;; https://shiny.rstudio.com/gallery/example-01-hello.html
 ;; Run this, and browse the url printed at the REPL.
 
-(base/print
+(r.base/print
  (s/shinyApp
   (s/shinyUI
    (s/fluidPage
@@ -17,14 +20,15 @@
       (s/sliderInput
        "obs"
        "Number of observations"
-       [:= :min 1]
-       [:= :max 1000]
-       [:= :value 500]))
+       :min 1
+       :max 1000
+       :value 500))
      (s/mainPanel
       (s/plotOutput "distPlot")))))
   (s/shinyServer
-   (r "function(input, output) {output$distPlot <- renderPlot(hist(rnorm(input$obs)))}"))))
-
+   (r '(function [input output]
+                 (<- output$distPlot
+                     (renderPlot (hist (rnorm input$obs)))))))))
 ;; To get the session to respond again, Ctrl+C at your REPL,
 ;; and then discard the current session running the Shiny app:
 
