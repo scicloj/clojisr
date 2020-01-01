c0b72bb6a9ec2608af2849dd37967abf8d45cfe6
daslu <daniel.slutsky@gmail.com>
2020-01-01 15:39:10 +0200

    notespace integration

---
+++ src/clojuress/v0/note.clj
@@ -0,0 +1,15 @@
+(ns clojuress.v0.note
+  (:require [notespace.v0.note :as note]
+            [clojure.walk :as walk]
+            [clojuress.v0 :as r]))
+
+(note/defkind note-r
+  :r  {:render-src?    true
+       :value-renderer (fn [value]
+                         (->> value
+                              (walk/postwalk
+                               (fn [v1]
+                                 (if (r/r-object? v1)
+                                   (r/r->clj v1)
+                                   v1)))
+                              note/value->html))})
