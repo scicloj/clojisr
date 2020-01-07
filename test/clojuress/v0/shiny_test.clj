318c7cac592c44ecac5d1b1503dbb49baa2bab3a
daslu <daniel.slutsky@gmail.com>
2020-01-07 19:33:06 +0200

    put blocking example code in comment

--- test/clojuress/v0/shiny_test.clj
+++ test/clojuress/v0/shiny_test.clj
@@ -10,27 +10,29 @@
 ;; https://shiny.rstudio.com/gallery/example-01-hello.html
 ;; Run this, and browse the url printed at the REPL.
 
-(r.base/print
- (s/shinyApp
-  (s/shinyUI
-   (s/fluidPage
-    (s/titlePanel "Hello Shiny!")
-    (s/sidebarLayout
-     (s/sidebarPanel
-      (s/sliderInput
-       "obs"
-       "Number of observations"
-       :min 1
-       :max 1000
-       :value 500))
-     (s/mainPanel
-      (s/plotOutput "distPlot")))))
-  (s/shinyServer
-   (r '(function [input output]
-                 (<- output$distPlot
-                     (renderPlot (hist (rnorm input$obs)))))))))
+(comment
+  (r.base/print
+   (s/shinyApp
+    (s/shinyUI
+     (s/fluidPage
+      (s/titlePanel "Hello Shiny!")
+      (s/sidebarLayout
+       (s/sidebarPanel
+        (s/sliderInput
+         "obs"
+         "Number of observations"
+         :min 1
+         :max 1000
+         :value 500))
+       (s/mainPanel
+        (s/plotOutput "distPlot")))))
+    (s/shinyServer
+     (r '(function [input output]
+                   (<- output$distPlot
+                       (renderPlot (hist (rnorm input$obs))))))))))
 ;; To get the session to respond again, Ctrl+C at your REPL,
 ;; and then discard the current session running the Shiny app:
 
-(r/discard-default-session)
+(comment
+  (r/discard-default-session))
 
