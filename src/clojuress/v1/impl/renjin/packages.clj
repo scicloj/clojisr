49da45aad54bb58234696cd5a791ff28de701a00
daslu <daniel.slutsky@gmail.com>
2020-02-05 09:56:40 +0200

    cleanup, fixed circular ns depencency

--- src/clojuress/v1/impl/renjin/packages.clj
+++ src/clojuress/v1/impl/renjin/packages.clj
@@ -1,20 +1,9 @@
 (ns clojuress.v1.impl.renjin.packages
   (:require [clojuress.v1.protocols :as prot]
-            [clojuress.v1.impl.renjin.engine :as engine]
-            [clojuress.v1.session :as session])
+            [clojuress.v1.impl.renjin.engine :as engine])
   (:import (org.renjin.sexp DynamicEnvironment ListVector$NameValuePair)
            (org.renjin.eval Context)
            (org.renjin.primitives.packaging NamespaceRegistry ClasspathPackageLoader ClasspathPackage)))
-
-(->> nil
-     session/fetch-or-make
-     :engine
-     ^Context
-     engine/runtime-context
-     (.getBaseEnvironment)
-     ;; (.getNames)
-     ;; (prot/java->clj session)
-     )
 
 (defn package-symbol->r-symbol-names [session package-symbol]
   (if (= package-symbol 'base)
