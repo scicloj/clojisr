3be88989ce48759de023490ca82bbe25bba09193
daslu <daniel.slutsky@gmail.com>
2020-03-29 11:35:03 +0300

    updated notespace dep version

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [scicloj/notespace "2.0.0-alpha3"]                 
-                 [com.rpl/specter "1.1.3"]
+                 [scicloj/notespace "2.0.0-alpha4"]
                  [techascent/tech.datatype "4.88"]
                  [techascent/tech.resource "4.6"]
                  [techascent/tech.ml.dataset "2.0-beta-4"]
